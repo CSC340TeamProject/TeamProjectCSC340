dac6a00dd3eda73fe9e44a6090f0d1b67efcda8f
bradywhite14 <jamesbwhite0@gmail.com>
2026-03-03 00:03:53 -0500

    created addEvent screen

--- TeamProjectCSC340/Form1.Designer.cs
+++ TeamProjectCSC340/Form1.Designer.cs
@@ -162,8 +162,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
